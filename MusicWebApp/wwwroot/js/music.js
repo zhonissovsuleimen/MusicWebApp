b8aa46d4fdbb3740b387f3fa205633521d9a5382
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-10-07 14:47:25 +0500

    changing page numebr

--- MusicWebApp/wwwroot/js/music.js
+++ MusicWebApp/wwwroot/js/music.js
@@ -1,16 +1,18 @@
 ﻿const table_body = document.getElementById('tableBody');
 const likes_view = document.getElementById('likesValue');
+const pagination = document.getElementById('pagination');
 
 const seed_input = document.getElementById('seedInput');
 const likes_input = document.getElementById('likesInput');
 const page_size_input = document.getElementById('pageSizeInput');
-
+  
 const ulong_max = 18446744073709551615n;
 const uint_max = 4294967295;
 const locale = "en";
 
 let cached_songs = new Map();
 let cached_likes = new Map();
+let page_id = 0;
 
 function is_valid_seed(str) {
   if (!/^\d+$/.test(str)) return false;
@@ -42,8 +44,18 @@ function is_valid_page_size(str) {
   }
 }
 
+function is_valid_page_number(str) {
+  if (!/^\d+$/.test(str)) return false;
+  try {
+    const n = parseInt(str, 10);
+    return n >= 0;
+  } catch {
+    return false;
+  }
+}
 
-async function redraw_table() {
+
+function redraw_table() {
   if (!table_body) {
     return;
   }
@@ -65,7 +77,7 @@ async function redraw_table() {
   table_body.innerHTML = tableHtml;
 }
 
-async function redraw_likes_input() {
+function redraw_likes_input() {
   if (!likes_view) {
     return;
   }
@@ -73,6 +85,43 @@ async function redraw_likes_input() {
   likes_view.innerHTML = likes_input.value;
 }
 
+function redraw_pagination() {
+  if (!pagination) {
+    return;
+  }
+
+  let html = '';
+  const disabled_status = page_id == 0 ? 'disabled' : '';
+  html += `<li class="page-item ${disabled_status}">
+        <button class="page-link" type="button" onclick="update_page_number(${page_id - 1})">
+        <span>&laquo;</span>
+        </button>
+      </li>`;
+
+  const renderPageButton = (i) => {
+    const activeClass = i === page_id ? 'active' : '';
+    return `<li class="page-item ${activeClass}"><button class="page-link" type="button" onclick="update_page_number(${i})">${i+1}</button></li>`;
+  };
+
+  if (page_id < 2) {
+    for (let i = 0; i < 5; i++) {
+      html += renderPageButton(i);
+    }
+  } else {
+    let start = page_id - 2;
+    for (let i = start; i < start + 5; i++) {
+      html += renderPageButton(i);
+    }
+  }
+
+  html += `<li class="page-item">
+        <button class="page-link" type="button" onclick="update_page_number(${page_id + 1})">
+        <span>&raquo;</span>
+      </button>
+      </li>`;
+
+  pagination.innerHTML = html;
+}
 
 async function fetch_songs(locale, seed, start, end) {
   try {
@@ -126,7 +175,6 @@ async function fetch_data(clean) {
   }
 
   const page_size = parseInt(page_size_str, 10);
-  const page_id = 0;
 
   const start = page_id * page_size;
   const end = start + page_size;
@@ -139,6 +187,20 @@ async function update() {
   await fetch_data(true);
   redraw_table();
   redraw_likes_input();
+  redraw_pagination();
+}
+
+async function update_page_number(page_id_str) {
+  if (!is_valid_page_number(page_id_str)) {
+    return;
+  }
+  page_id = parseInt(page_id_str, 10);
+  update();
+}
+
+async function update_page_size() {
+  page_id = 0;
+  update();
 }
 
 function randomize_seed() {
